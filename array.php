450f85faf429cf9a2e8f9b52c5cceeec69a1d7f8
RobiulHassan0 <robiulhassan270@gmail.com>
2025-06-24 23:58:39 +0600

    more explor arr

--- array.php
+++ array.php
@@ -11,3 +11,16 @@ var_dump($fruits);
 var_dump($vegetables);
 // Output:
 
+$names = array('robiul', 'hassan', 'robin', 'ibn', 'fulan');
+// Output the array using var_dump
+var_dump($names);
+// Output: array(5) { [0]=> string(5) "robiul
+echo $names[2]; // Output: robin
+// Accessing elements in an array using their index
+
+$names[3] = 'ibn'; // Assigning a new value to an existing index
+echo $names[3]; // Output: ibn
+// Adding a new element to the array
+$names[] = 'newName'; // Automatically assigns the next index
+echo $names[5]; // Output: newName
+// Output the entire array after modification
