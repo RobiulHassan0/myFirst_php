6ce40771380f862575cd40ea46d3694a48f58b64
RobiulHassan0 <robiulhassan270@gmail.com>
2025-06-24 23:09:08 +0600

    explore array in php

---
+++ array.php
@@ -0,0 +1,13 @@
+<?php
+// Array style syntax in PHP
+// PHP supports two styles of array syntax: the traditional `array()` function and the short array syntax `[]`
+// The short array syntax is preferred in modern PHP code for its simplicity and readability.
+//// Example of traditional array syntax
+$fruits = array('Apple', 'Banana', 'Cherry');
+// Example of short array syntax
+$vegetables = ['Carrot', 'Potato', 'Tomato'];
+// Output the arrays
+var_dump($fruits);
+var_dump($vegetables);
+// Output:
+
