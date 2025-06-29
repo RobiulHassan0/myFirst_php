2e869f28b63a564090d4ff4629c59a6b8706ef06
RobiulHassan0 <robiulhassan270@gmail.com>
2025-06-29 23:59:38 +0600

    more

--- function.php
+++ function.php
@@ -7,9 +7,26 @@ sayHi();
 
 echo "\n"; //line break using \n
 
-function sayHello($name){
-    echo "Hello, $name!";
+function sayHello($name, $age){
+    echo "Hello, $name!", $age;
 }
-sayHello("Robiul Hassan");
+sayHello("Robiul Hassan", 25);
 echo PHP_EOL; // line break using PHP_EOL
-sayHello("ibn fulan");
+sayHello("ibn fulan", 30);
+
+echo "\n";
+
+function add($a, $b){
+    // This function adds two numbers and prints the result
+    // Parameters: $a and $b are the numbers to be added
+    echo $a + $b;
+}
+add(2, 4);
+
+echo "\n";
+
+function addNumbers($a, $b){
+    $add = $a + $b; // This function adds two numbers and prints the result
+    echo "The sum of $a and $b is: " . $add . "\n";
+}
+addNumbers(5, 10);
