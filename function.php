5e81bc52b72f1a65f6203c8ca73809cae7bf0a60
RobiulHassan0 <robiulhassan270@gmail.com>
2025-06-29 23:44:27 +0600

    explore function

--- function.php
+++ function.php
@@ -4,3 +4,12 @@ function sayHi(){
     echo "Hi, PHP!";
 }
 sayHi();
+
+echo "\n"; //line break using \n
+
+function sayHello($name){
+    echo "Hello, $name!";
+}
+sayHello("Robiul Hassan");
+echo PHP_EOL; // line break using PHP_EOL
+sayHello("ibn fulan");
